2f8ca379b06bf59ffd6162f45e3f8b77e68f3f62
Akshith Mothkuri <amothkuri@wisc.edu>
2024-09-21 20:58:32 +0000

    Initial commit of project files

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+docker build . -t p1
+docker run p1
+
---
+++ count.sh
@@ -0,0 +1,3 @@
+curl -O https://pages.cs.wisc.edu/~harter/cs544/data/hdma-wi-2021.zip
+unzip hdma-wi-2021.zip
+grep -c "Multifamily" hdma-wi-2021.csv
