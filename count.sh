7564b4a9e9dfc76981a14cbf66333f830b1f3e63
Akshith Mothkuri <amothkuri@wisc.edu>
2024-09-24 03:03:23 +0000

    modified

--- count.sh
+++ count.sh
@@ -1,3 +1,4 @@
-curl -O https://pages.cs.wisc.edu/~harter/cs544/data/hdma-wi-2021.zip
-unzip hdma-wi-2021.zip
+#! /usr/bin/bash
+wget https://pages.cs.wisc.edu/~harter/cs544/data/hdma-wi-2021.zip
+unzip -o hdma-wi-2021.zip
 grep -c "Multifamily" hdma-wi-2021.csv
